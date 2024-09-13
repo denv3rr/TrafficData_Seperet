2f7ee769705a691d0efeeeb6a41c78b9794b0835
denv3rr <denver.clark@g.austincc.edu>
2024-09-13 00:08:05 -0500

    added menu text

--- menu.cpp
+++ menu.cpp
@@ -6,3 +6,18 @@
 #include <iostream>
 #include "menu.h"
 #include "traffic_data.h"
+
+/*
+ * displayMenu() function - self explanitory
+ */
+void displayMenu()
+{
+    std::cout << "Internet Traffic Auditor\n\n";
+    std::cout << "1. Add Traffic Data\n";
+    std::cout << "2. View Traffic Data\n";
+    std::cout << "3. Visit Seperet.com\n";
+    std::cout << "4. Visit Our YouTube Channel\n";
+    std::cout << "5. Exit\n";
+    std::cout << "\n";
+    std::cout << "Enter selection:\n";
+}
