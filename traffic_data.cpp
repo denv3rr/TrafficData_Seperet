a3f2d269bcff63ab1bcd0e5d30e090d25f058efc
denv3rr <denver.clark@g.austincc.edu>
2024-09-13 00:57:29 -0500

    added form for entering new data manually for meantime

--- traffic_data.cpp
+++ traffic_data.cpp
@@ -4,12 +4,24 @@
  *
  */
 #include <iostream>
+#include <fstream>
 #include "traffic_data.h"
 
 std::vector<TrafficData> trafficDataList;
 
-void addTrafficData() {
-
+void addTrafficData()
+{
+    TrafficData data;
+    std::cout << "Enter URL: ";
+    std::cin >> data.url;
+    std::cout << "Enter number of visitors: ";
+    std::cin >> data.visitors;
+    std::cout << "Enter number of page views: ";
+    std::cin >> data.pageViews;
+    std::cout << "Enter bandwidth usage (in MB): ";
+    std::cin >> data.bandwidth;
+    trafficDataList.push_back(data);
+    std::cout << "Traffic data added successfully.\n";
 };
 
 void viewTrafficData() {
