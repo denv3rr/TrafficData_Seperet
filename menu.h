b98217a7b0ac8a031ea80ca25223acf30155a1d7
denv3rr <denver.clark@g.austincc.edu>
2024-09-13 17:57:11 -0500

    added a menu state enum and left comment for future menu additions as needed to be added

--- menu.h
+++ menu.h
@@ -6,6 +6,18 @@
 #ifndef MENU_H
 #define MENU_H
 
+enum MenuState
+{
+    MAIN_MENU,
+    COMPANY_INFO_MENU,
+    /*
+     * this is where new menu states
+     * can be added if you need them
+     * at any point
+     *
+     */
+};
+
 void displayMenu();
 void handleMenuSelection(int choice);
 
