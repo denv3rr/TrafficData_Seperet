706dd7bbd68c0b990600fd7f64b92e233d6f3051
denv3rr <denver.clark@g.austincc.edu>
2024-09-13 00:49:01 -0500

    traffic data header file function prototype added for manually entering new data - this will be changed at some point

--- traffic_data.h
+++ traffic_data.h
@@ -2,3 +2,21 @@
  * traffic_data.cpp header file
  *
  */
+#ifndef TRAFFIC_DATA_H
+#define TRAFFIC_DATA_H
+
+#include <vector>
+#include <string>
+
+struct TrafficData
+{
+    std::string url;
+    int visitors;
+    int pageViews;
+    double bandwidth; // in MB
+};
+
+void addTrafficData();
+void viewTrafficData();
+
+#endif // TRAFFIC_DATA_H
